c1f2e4ef9be803e23c88c542ae17d5634ea6678a
Wendell <165390473+wendellbernini@users.noreply.github.com>
2024-12-11 09:47:06 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base', // Regras do Airbnb
+    'plugin:prettier/recommended', // Integração com Prettier
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'], // Adiciona Prettier como plugin
+  rules: {
+    'prettier/prettier': 'error', // Marca como erro qualquer desvio das regras do Prettier
+    // Limita a complexidade de funções/métodos
+    complexity: ['error', 5],
+
+    // Limita o número de linhas em uma função/arquivo
+    'max-lines': [
+      'error',
+      { max: 50, skipBlankLines: true, skipComments: true },
+    ],
+
+    // Limita o número de declarações em uma função
+    'max-statements': ['error', 10],
+
+    // Previne o uso de números "mágicos"
+    'no-magic-numbers': ['error', { ignore: [0, 1] }],
+
+    // Evita ternários aninhados
+    'no-nested-ternary': 'error',
+
+    // Limita o número de linhas em branco consecutivas
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+  },
+};
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            targets: 'defaults',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/utils/index.html',
+    }),
+  ],
+};
